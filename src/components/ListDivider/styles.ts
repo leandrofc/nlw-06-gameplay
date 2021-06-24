6de00ed42dd759c5bc24555daa2f5cfa92ab82df
leandrofc <leandromk@outlook.com>
2021-06-24 16:49:51 -0300

    resultado da aula 03

--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -5,10 +5,11 @@ import {
 
 export const styles = StyleSheet.create({
   container: {
-    width: '80%',
+    width: '75%',
     height: 1,
     backgroundColor: theme.colors.secondary40,
-    marginVertical: 21,
+    marginTop: 2,
+    marginVertical: 31,
     alignSelf: 'flex-end'
   },
 });
